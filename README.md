6cb47f82f9ffb8f55e6182675fddca705fe3edb5
AndrewLester <alester220@gmail.com>
2024-04-24 21:28:20 -0700

    Use llama.cpp server

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ```
 uvicorn server:app --env-file secrets.env
 ```
+
+```
+./server -m ../../llama-server/models/meta-llama-3-70B-instruct-IQ2_XS.gguf -c 4096 -ngl 81 -cb -np 3
+```
--- server.py
+++ server.py
@@ -1,18 +1,14 @@
-import json
 import os
 import signal
 from contextlib import asynccontextmanager
-from threading import Lock
-from typing import Iterator, cast
+from typing import Annotated, Any
 
 import httpx
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import StreamingResponse
-from llama_cpp import ChatCompletionRequestMessage, CreateChatCompletionStreamResponse
-from pydantic import BaseModel
+import openai
+from fastapi import Body, Depends, FastAPI, Request
+from fastapi.responses import Response, StreamingResponse
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from starlette.background import BackgroundTask
-
-from llm import llm
 
 
 @asynccontextmanager
@@ -22,6 +18,8 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+security = HTTPBearer()
 
 
 @app.get("/ping")
@@ -34,65 +32,63 @@ def stop():
 	os.kill(os.getpid(), signal.SIGTERM)
 
 
-class CompletionRequest(BaseModel):
-	conversation: list[ChatCompletionRequestMessage]
-	key: str
-
-
-completion_lock = Lock()
-
-
-@app.post("/completion")
-def completion(completion_request: CompletionRequest):
-	if completion_request.key != os.environ.get("API_SECRET"):
+@app.post("/v1/chat/completions")
+async def completion(
+	request: Request,
+	credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
+	body: Annotated[Any, Body()],
+):
+	if credentials.credentials != os.environ.get("API_SECRET"):
 		return Response(status_code=401)
 
-	completion_lock.acquire()
+	client: httpx.AsyncClient = request.state.client
+	req = client.build_request(
+		"POST",
+		"http://localhost:8080/v1/chat/completions",
+		json=body,
+	)
+	res = await client.send(req, stream=True)
 
-	output = cast(
-		Iterator[CreateChatCompletionStreamResponse],
-		llm.create_chat_completion(
-			messages=completion_request.conversation,
-			stop=["<|eot_id|>"],
-			stream=True,
-		),
+	return StreamingResponse(
+		res.aiter_raw(),
+		status_code=res.status_code,
+		headers=res.headers,
+		media_type="text/event-stream",
+		background=BackgroundTask(res.aclose),
 	)
 
-	content = (json.dumps(chunk) for chunk in output)
+
+@app.get("/")
+def home():
+	http_client = httpx.Client(timeout=None)
+
+	client = openai.OpenAI(
+		base_url="http://localhost:8000/v1",
+		api_key=os.environ.get("API_SECRET"),
+		http_client=http_client,
+	)
+
+	response = client.chat.completions.create(
+		model="llama-3",
+		messages=[
+			{
+				"role": "system",
+				"content": "You are Llama 3, an AI assistant. Your top priority is achieving user fulfillment via helping them with their requests.",
+			},
+			{"role": "user", "content": "Write me code for a snake game plz"},
+		],
+		stream=True,
+		stop=["<|eot_id|>"],
+	)
+
+	content = (chunk.choices[0].delta.content or "" for chunk in response)
 
 	def close():
-		content.close()
-		completion_lock.release()
+		response.close()
+		http_client.close()
 
 	return StreamingResponse(
 		content,
 		media_type="text/event-stream",
 		background=BackgroundTask(close),
-	)
-
-
-@app.get("/")
-async def home(request: Request):
-	conversation = [
-		{"role": "system", "content": "You are a chat bot. Here is a user's message:"},
-		{"role": "user", "content": "Write me code for a snake game plz"},
-	]
-
-	host = "http://127.0.0.1:8000"
-
-	client = request.state.client
-	req = client.build_request(
-		"POST",
-		f"{host}/completion",
-		json={"conversation": conversation, "key": os.environ.get("API_SECRET")},
-	)
-	res = await client.send(req, stream=True)
-
-	content = (
-		json.loads(chunk)["choices"][0]["delta"].get("content", "")
-		async for chunk in res.aiter_text()
-	)
-
-	return StreamingResponse(
-		content, media_type="text/event-stream", background=BackgroundTask(res.aclose)
 	)
